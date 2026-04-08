da434d9f8ed323e186b157a95fcd5ded46460f9e
Feiyang Ning <24s051041@stu.hit.edu.cn>
2026-04-08 16:48:12 +0800

    Update main_phase3.py

--- main_phase3.py
+++ main_phase3.py
@@ -54,17 +54,15 @@ def train(epoch, dataloader, lambda_2):
         inputs, targets = inputs.cuda(), targets.cuda()
         outputs = model(inputs)
         _, predicted = outputs.max(1)
+
+        total_loss = torch.tensor(0.0, requires_grad=True, device='cuda')
         
         """ 
-        total_loss = torch.tensor(0.0, requires_grad=True).cuda()
-        
         clean_mask = total_clean_idx[index].cuda()
         if clean_mask.sum() > 0:
             loss_clean = criterion(outputs[clean_mask], targets[clean_mask]).mean()
-            total_loss += loss_clean
+            total_loss = total_loss + loss_clean
         """
-        
-        total_loss = 0.0
         
         alpha = 0.20
         clean_mask = total_clean_idx[index].cuda()
@@ -87,7 +85,7 @@ def train(epoch, dataloader, lambda_2):
                 (1 - lam) * criterion(outputs_clean, targets_b)
             ).mean()
 
-            total_loss += loss_clean
+            total_loss = total_loss + loss_clean
         
         high_conf_mask = torch.zeros_like(clean_mask, dtype=torch.bool)
         soft_targets = []
@@ -114,7 +112,7 @@ def train(epoch, dataloader, lambda_2):
                 kl_qp = F.kl_div(log_targets, preds_clamped, reduction='batchmean')
 
                 sym_kl = 0.5 * (kl_pq + kl_qp)
-                total_loss += lambda_2 * sym_kl      
+                total_loss = total_loss + lambda_2 * sym_kl      
         
         total_loss.backward()
         optimizer.step()
